d99ce9129fe8c99689ad7af51a0c625318ad6e25
adsroor <3459088548@qq.com>
2020-11-19 23:22:46 +0800

    swiper的修改

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -5,12 +5,8 @@
   <swiper :options="swiperOption" v-if="showSwiper">
       <swiper-slide v-for="item of list" :key="item.id">
       <img class="swiper-img " :src="item.imgUrl"/>
-
       </swiper-slide>
-
-
       <div class="swiper-pagination"  slot="pagination"></div>
-
    </swiper>
   </div>
 </template>
